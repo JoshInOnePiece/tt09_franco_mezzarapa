916e1ad82185b57a1fbf9244267984227f07c234
Joshua Joseph <jo634076@ucf.edu>
2024-11-05 12:10:46 -0500

    Small testbench

--- test/test.py
+++ test/test.py
@@ -12,21 +12,28 @@ async def test_project(dut):
     # Set the clock period to 10 us (100 MHz)
     clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
-    await ClockCycles(dut.clk, 80)
+    await ClockCycles(dut.clk, 2)
+
+    dut._.log.ingo("Reset")
+    dut.clk.value = 0
+    dut.ena.value = 0
+    dut.rst_n.value = 1
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 10)
 
     #Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-    dut._log.info("Test project behavior")
+    # dut._log.info("Reset")
+    # dut.ena.value = 1
+    # dut.ui_in.value = 0
+    # dut.uio_in.value = 0
+    # dut.rst_n.value = 0
+    # await ClockCycles(dut.clk, 10)
+    # dut.rst_n.value = 1
+    # dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # # Set the input values you want to test
+    # dut.ui_in.value = 20
+    # dut.uio_in.value = 30
 
 # @cocotb.test()
 # async def test_project(dut):
